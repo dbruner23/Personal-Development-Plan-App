a2f5fbd2a5736c46fe1455bebb8bdb80d3032873
Sonja Fantom <sonja.fantom@gmail.com>
2022-12-01 16:43:59 +1300

    connected feedback modal to db and updated prototypes

--- src/components/prototypes/s5/SankeyChart.tsx
+++ src/components/prototypes/s5/SankeyChart.tsx
@@ -3,6 +3,21 @@ import Node from "./Node";
 import Link from "./Link";
 import sankeyData from "./sankeyData";
 import sankeyData2 from "./sankeyData2";
+import sankeyData3 from "./sankeyData3";
+
+
+const CustomTooltip = ({ active, payload }: any) => {
+  if (active) {
+    return (
+      <div className="bg-[#ffffff] p-2 border-2 text-center ">
+        <p>{`To progress from ${payload?.[0].name}`}</p>
+        <p>{`will take you around ${payload?.[0].value} months`}</p>
+      </div>
+    );
+  }
+
+  return null;
+};
 
 
 
@@ -28,8 +43,8 @@ const SankeyChart = () => {
           <stop offset="100%" stopColor="rgba(52, 77, 186, 0.3)" />
         </linearGradient>
       </defs>
-      {/* <Tooltip content={<CustomTooltip payload={sankeyData} />} /> */}
-      <Tooltip />
+      {/* <Tooltip /> */}
+      <Tooltip content={<CustomTooltip />} />
     </Sankey>
     </ResponsiveContainer>
     </>
